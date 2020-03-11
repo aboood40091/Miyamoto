b24b63b190662ea6ca2af71c2a4484593aa8840b
mrbengtsson <16099910+mrbengtsson@users.noreply.github.com>
2020-03-11 12:14:32 +0100

    Merge Spike Pillars & More (#92)
    * Spritedata and images added for all spike pillars
    + Minor spritedata changes
    
    * Added long continuous burner
    * ZoomArea aux added
    * Castle platform and chain holder spriteimages added
    * Lantern platform added
    
    * Bolt-controlled stone block added
    + 580: Stone spike
    
    * Stone blocks from Morty's Castle added
    
    * Added Ghost Ship Block
    + 386: Meltable Ice Chunkn
    
    * Scale platform (308, 309) added
    
    * Several sprites updated
    - 31: Updated goal pole images (the castle is now an aux)
    + 263: Added pipe bubbles image
    + 559: Aux added
    - 588: Renamed to Deep Cheep
    + 590: Image added for Eep Cheep
    
    * Metal bridge sprites and ghost house pole added
    - Muncher and star platform image update
    
    * Read the comments
    +276: Jellybeam added
    +321: Bulber added
    +324: Ghost house block added
    +341: Airship Nut added
    +482: Mecha Cheep added
    +550: Giant Pipe Piranha added

--- spritelib.py
+++ spritelib.py
@@ -664,6 +664,7 @@ class AuxiliaryImage(AuxiliarySpriteItem):
         self.height = height
         self.image = None
         self.hover = True
+        self.opacity = 1
 
     def setSize(self, width, height, xoff=0, yoff=0):
         self.prepareGeometryChange()
@@ -678,7 +679,9 @@ class AuxiliaryImage(AuxiliarySpriteItem):
             painter.setClipRect(option.exposedRect)
 
         if self.image is not None:
+            painter.setOpacity(self.opacity)
             painter.drawPixmap(0, 0, self.image)
+            painter.setOpacity(1)
 
 
 class AuxiliaryImage_FollowsRect(AuxiliaryImage):
