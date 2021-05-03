aboood40091/Miyamoto

commit 437228c0764b4d474283b9cfda6e0d7bf3caeed6

Fix objects deselecting when switching from objects tab to another tab

miyamoto.py
@@ -4243,18 +4243,17 @@ class MiyamotoWindow(QtWidgets.QMainWindow):
             SetDirty()
         self.levelOverview.update()
 
-    def CreationTabChanged(self, nt):
+    def CreationTabChanged(self, idx):
         """
         Handles the selected palette tab changing
         """
-        idx = self.creationTabs.currentIndex()
         CPT = -1
         if idx == 0:  # objects
             CPT = self.objAllTab.currentIndex()
             if CPT == 1:
                 CPT = 10 # "All" objects
-        elif idx == 1:  # sprites
-            if self.sprAllTab.currentIndex() != 1: CPT = 4
+        elif idx == 1 and self.sprAllTab.currentIndex() != 1:  # sprites
+            CPT = 4
         elif idx == 2:
             CPT = 5  # entrances
         elif idx == 3:
@@ -4268,8 +4267,17 @@ class MiyamotoWindow(QtWidgets.QMainWindow):
         elif idx == 8:
             CPT = 9  # comment
 
+        type = -1
+        if CPT in (0, 2, 10):
+            index = self.objPicker.currentIndex()
+            if index.isValid():
+                if CPT == 2:
+                    type, CPT = self.objTS123Tab.getObjectAndPaintType(index.row())
+                else:
+                    type = index.row()
+
         globals.CurrentPaintType = CPT
-        globals.CurrentObject = -1
+        globals.CurrentObject = type
 
     def ObjTabChanged(self, nt):
         """
@@ -4523,7 +4531,7 @@ class MiyamotoWindow(QtWidgets.QMainWindow):
         """
         Handles a new object being chosen
         """
-        if globals.CurrentPaintType not in [0, 10]:
+        if globals.CurrentPaintType not in (0, 10):
             globals.CurrentObject, globals.CurrentPaintType = self.objTS123Tab.getObjectAndPaintType(type)
 
         else:

widgets.py
@@ -1939,6 +1939,7 @@ class ObjectPickerWidget(QtWidgets.QListView):
             self.itemsize = []
             self.tooltips = []
 
+            mainWindow = globals.mainWindow
             self.beginResetModel()
 
             globals.numObj = []
@@ -2009,6 +2010,7 @@ class ObjectPickerWidget(QtWidgets.QListView):
                 globals.numObj.append(z)
 
             self.endResetModel()
+            mainWindow.CreationTabChanged(mainWindow.creationTabs.currentIndex())
 
         def LoadFromFolder(self):
             """
@@ -2023,11 +2025,13 @@ class ObjectPickerWidget(QtWidgets.QListView):
             self.itemsize = []
             self.tooltips = []
 
+            mainWindow = globals.mainWindow
             self.beginResetModel()
 
             # Fixes issues if the user selects the wrong Objects Folder
             if not globals.mainWindow.folderPicker.currentText():
                 self.endResetModel()
+                mainWindow.CreationTabChanged(mainWindow.creationTabs.currentIndex())
                 return
 
             z = 0
@@ -2182,6 +2186,7 @@ class ObjectPickerWidget(QtWidgets.QListView):
                 z += 1
 
             self.endResetModel()
+            mainWindow.CreationTabChanged(mainWindow.creationTabs.currentIndex())
 
 
 class StampChooserWidget(QtWidgets.QListView):

commit 5930ca4673e66eea1f6b5374390ae8bb584edfb0

Clear selection of object picker when tileset is edited
Additionally, fix a bug caused by commit 437228c0764b4d474283b9cfda6e0d7bf3caeed6 (Fix objects deselecting when switching from objects tab to another tab).

miyamoto.py
@@ -3811,7 +3811,7 @@ class MiyamotoWindow(QtWidgets.QMainWindow):
             self.LoadLevel_NSMBU(levelData, areaNum)
 
         # Refresh object layouts
-        self.objPicker.LoadFromTilesets()
+        HandleTilesetEdited(True)
         for layer in globals.Area.layers:
             for obj in layer:
                 obj.updateObjCache()
@@ -3896,8 +3896,6 @@ class MiyamotoWindow(QtWidgets.QMainWindow):
         self.objUseLayer1.setChecked(True)
 
         self.ReloadTilesets()
-
-        self.objPicker.LoadFromTilesets()
 
         self.objAllTab.setCurrentIndex(0)
         self.objAllTab.setTabEnabled(0, True)
@@ -4014,7 +4012,7 @@ class MiyamotoWindow(QtWidgets.QMainWindow):
             if (name is not None) and (name != ''):
                 LoadTileset(idx, name, not soft)
 
-        self.objPicker.LoadFromTilesets()
+        HandleTilesetEdited(True)
 
         for layer in globals.Area.layers:
             for obj in layer:
@@ -5168,7 +5166,7 @@ class MiyamotoWindow(QtWidgets.QMainWindow):
                 globals.Area.tileset0 = ''
                 UnloadTileset(0)
 
-            self.objPicker.LoadFromTilesets()
+            HandleTilesetEdited(True)
 
             if globals.Area.tileset0 != '':
                 self.objAllTab.setCurrentIndex(0)
@@ -5427,7 +5425,7 @@ class MiyamotoWindow(QtWidgets.QMainWindow):
 
                 LoadTileset(0, globals.Area.tileset0)
                 SetDirty()
-                self.objPicker.LoadFromTilesets()
+                HandleTilesetEdited(True)
 
                 if globals.Area.tileset0 != '':
                     self.objAllTab.setCurrentIndex(0)

tileset.py
@@ -1095,12 +1095,15 @@ def exportObject(name, baseName, idx, objNum):
         json.dump(jsonData, outfile)
 
 
-def HandleTilesetEdited(fromPuzzle=False):
-    if not fromPuzzle:
+def HandleTilesetEdited(soft=False):
+    if not soft:
         globals.TilesetEdited = True
 
-    globals.mainWindow.objPicker.LoadFromTilesets()
-    globals.mainWindow.updateNumUsedTilesLabel()
+    mainWindow = globals.mainWindow
+    mainWindow.objPicker.clearSelection()
+    mainWindow.objPicker.LoadFromTilesets()
+    mainWindow.updateNumUsedTilesLabel()
+    mainWindow.CreationTabChanged(mainWindow.creationTabs.currentIndex())
 
 
 def DeleteObject(idx, objNum, soft=False):

widgets.py
@@ -1939,7 +1939,6 @@ class ObjectPickerWidget(QtWidgets.QListView):
             self.itemsize = []
             self.tooltips = []
 
-            mainWindow = globals.mainWindow
             self.beginResetModel()
 
             globals.numObj = []
@@ -2010,7 +2009,6 @@ class ObjectPickerWidget(QtWidgets.QListView):
                 globals.numObj.append(z)
 
             self.endResetModel()
-            mainWindow.CreationTabChanged(mainWindow.creationTabs.currentIndex())
 
         def LoadFromFolder(self):
             """
@@ -2025,13 +2023,11 @@ class ObjectPickerWidget(QtWidgets.QListView):
             self.itemsize = []
             self.tooltips = []
 
-            mainWindow = globals.mainWindow
             self.beginResetModel()
 
             # Fixes issues if the user selects the wrong Objects Folder
             if not globals.mainWindow.folderPicker.currentText():
                 self.endResetModel()
-                mainWindow.CreationTabChanged(mainWindow.creationTabs.currentIndex())
                 return
 
             z = 0
@@ -2186,7 +2182,6 @@ class ObjectPickerWidget(QtWidgets.QListView):
                 z += 1
 
             self.endResetModel()
-            mainWindow.CreationTabChanged(mainWindow.creationTabs.currentIndex())
 
 
 class StampChooserWidget(QtWidgets.QListView):